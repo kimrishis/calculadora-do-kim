52a5abf52f31c8c8cf191d3987b05f6d8ceb2c01
kim <kimrishis@gmail.com>
2022-05-13 19:09:18 -0300

    calculadora

--- scripts.js
+++ scripts.js
@@ -1,10 +1,25 @@
 var inputs = document.querySelectorAll("input")
 var inp1 = inputs[0]
 var inp2 = inputs[1]
-var button = document.querySelector("button")
+var buttons = document.querySelectorAll("button")
 
-button.addEventListener("click", function(){
+
+buttons[0].addEventListener("click", function(){
     var soma = parseFloat(inp1.value) + parseFloat(inp2.value)
     window.alert("resultado :> "+soma)
 })
 
+buttons[1].addEventListener("click", function(){
+    var subtracao = parseFloat(inp1.value) - parseFloat(inp2.value)
+    window.alert("resultado :> "+subtracao)
+})
+
+buttons[2].addEventListener("click", function(){
+    var multiplicacao = parseFloat(inp1.value) * parseFloat(inp2.value)
+    window.alert("resultado :> "+multiplicacao)
+})
+
+buttons[3].addEventListener("click", function(){
+    var divisao = parseFloat(inp1.value) / parseFloat(inp2.value)
+    window.alert("resultado :> "+divisao)
+})
